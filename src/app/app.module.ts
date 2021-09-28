ead62a418d136c2f1c0a1f29d7a225ab55818c44
sebastian.soderholm <sebastian.soderholm@fi.experis.com>
2021-09-28 14:16:31 +0300

    added trainer & catalogue modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StartComponent } from './components/start/start.component';
+import { TrainerComponent } from './components/trainer/trainer.component';
+import { CatalogueComponent } from './catalogue/catalogue.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartComponent,
+    TrainerComponent,
+    CatalogueComponent
   ],
   imports: [
     BrowserModule,
