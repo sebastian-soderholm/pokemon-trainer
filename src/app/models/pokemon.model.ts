36b4e21344996a3f40720f445313861be7381209
sebastian.soderholm <sebastian.soderholm@fi.experis.com>
2021-09-29 13:42:47 +0300

    added catalog more info fetch functionality

--- src/app/models/pokemon.model.ts
+++ src/app/models/pokemon.model.ts
@@ -1,5 +1,12 @@
 export interface Pokemon {
   id: number;
   name: string;
-  imageURL: string //Using obj.sprites.front_default image
+  infoURL: string;
+  imageURL: string; //Using obj.sprites.front_default image
+  info: PokemonInfo | undefined;
+}
+
+export interface PokemonInfo {
+  height: number;
+  weight: number;
 }
--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,30 +1,82 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { getAttrsForDirectiveMatching } from '@angular/compiler/src/render3/view/util';
 import { Injectable } from '@angular/core';
-import { Pokemon } from '../models/pokemon.model';
+import { Pokemon, PokemonInfo } from '../models/pokemon.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PokemonService {
-  private _apiURL: string = "https://pokeapi.co/api/v2";
+  private _apiURL: string = 'https://pokeapi.co/api/v2';
+  private _imgApiURL: string = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
   private _pokemons: Pokemon[] = [];
   private _error: string = '';
+  private _pokemonURLs: string[] = [];
 
-  constructor(private readonly http: HttpClient){}
+  constructor(private readonly http: HttpClient) {}
 
   public fetchPokemons(limit: number, offset: number): void {
-    this.http.get<Pokemon[]>(`${this._apiURL}/pokemon?limit=${limit}&offset=${offset}`)
-    .subscribe((returnObj: any) => {
-        this._pokemons = returnObj.results
-      }, (error: HttpErrorResponse) => {
+    this.http
+      .get(`${this._apiURL}/pokemon?limit=${limit}&offset=${offset}`)
+      .subscribe(
+        (returnObj: any) => {
+          //Loop through results to get URLs
+          this._pokemonURLs = returnObj.results.map((element: any, index: number) => {
+            this._pokemons.push({
+              id: index + offset + 1,
+              name: element.name,
+              infoURL: element.url,
+              imageURL: `${this._imgApiURL}${index + offset + 1}.png`,
+              info: undefined,
+            });
+          });
+        },
+        (error: HttpErrorResponse) => {
+          this._error = error.message;
+        }
+      );
+  }
+  // public fetchPokemon(pokemonId: number) {
+  //   //Fetch each pokemon one by one
+  //   this.http.get<Pokemon>(`${this._apiURL}/pokemon/${pokemonId}/`).subscribe(
+  //     (pokemonData: any) => {
+  //       this._pokemons.push({
+  //         id: pokemonData.pokemonId,
+  //         name: pokemonData.name,
+  //         infoURL: pokemonData.url,
+  //         imageURL: `${this._imgApiURL}${pokemonId}.png`,
+  //         pokemonInfo: null,
+  //       });
+  //     },
+  //     (error: HttpErrorResponse) => {
+  //       this._error = error.message;
+  //     }
+  //   );
+  // }
+  public fetchPokemonInfo(pokemonId: number) {
+    //Fetch pokemon's info
+    this.http.get<Pokemon>(`${this._apiURL}/pokemon/${pokemonId}/`).subscribe(
+      (pokemonData: any) => {
+        const pokemonIndex = this._pokemons.findIndex(pokemon => pokemon.id === pokemonId);
+        this._pokemons[pokemonIndex].info = <PokemonInfo> {
+          height: pokemonData.height,
+          weight: pokemonData.weight
+        }
+      },
+      (error: HttpErrorResponse) => {
         this._error = error.message;
-      });
+      }
+    );
   }
   public getPokemons(): Pokemon[] {
-    return this._pokemons
+    return this._pokemons;
+  }
+  public getPokemon(id: number): Pokemon {
+    const pokemonIndex = this._pokemons.findIndex(pokemon => pokemon.id === id);
+    return this._pokemons[pokemonIndex]
   }
   public error(): string {
-    return this._error
+    return this._error;
   }
 
   // PokemonAPI = {
