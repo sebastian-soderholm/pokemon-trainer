598c4015a8b5ecdd203fd4856914bee928097f40
sebastian.soderholm <sebastian.soderholm@fi.experis.com>
2021-09-30 12:33:38 +0300

    Added app.component styling to global scope

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  //Add app-root styles to global scope
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'pokemon-trainer';
