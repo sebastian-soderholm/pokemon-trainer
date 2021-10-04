9c4557d122d55354258a3b7985fe0607516503e8
Sergei Kaukiainen <sergei.kaukiainen@5CG04676KY>
2021-10-04 10:30:48 +0300

    Add style for the page

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -7,11 +7,15 @@ import { SessionService } from 'src/app/services/session.service';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  public loggedIn: boolean = false
 
   constructor(private readonly sessionService: SessionService) { }
 
   ngOnInit(): void {
+    this.loggedIn = this.sessionService.loggedIn 
   }
+
+  //loggedIn: boolean = this.sessionService.loggedIn 
 
   public logOut(): void {
     this.sessionService.logout()
